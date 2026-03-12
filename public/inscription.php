0927482ea2691fb3bcc42e0f7b5c647ab57ee5e8
Guidetti Gaspard <gaspardguidetti@gmail.com>
2026-03-12 17:01:04 +0100

    nettoyage css de inscription

--- public/inscription.php
+++ public/inscription.php
@@ -70,6 +70,7 @@ $liste_roles = $roles_query->fetchAll();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" >
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="styles/style_inscription.css">
+    <link rel="stylesheet" href="styles/main_theme.css">
     
 </head>
 <body>
