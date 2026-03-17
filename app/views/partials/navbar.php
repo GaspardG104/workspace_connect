e4625a92421a8951d6f865391fe3a22df29b9e8b
Guidetti Gaspard <gaspardguidetti@gmail.com>
2026-03-17 16:52:11 +0100

    index au model mvc

--- app/views/partials/navbar.php
+++ app/views/partials/navbar.php
@@ -1,6 +1,6 @@
  <nav class="navbar navbar-expand-lg navbar-dark shadow-sm">
         <div class="container">
-            <a class="navbar-brand fw-bold" href="/home">
+            <a class="navbar-brand fw-bold" href="/workspace_connect/home">
                 <i class="fa-solid fa-car-side me-2"></i> Workspace Connect
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -11,10 +11,10 @@
                 <ul class="navbar-nav ms-auto align-items-center">
                     <?php if (isset($_SESSION['user_id'])): ?>
                         <li class="nav-item">
-                            <a class ="nav-link btn text-white px-3 me-2 btn-sm" href="/account/account.php"><i class="fa-regular fa-circle-user"></i> <?= htmlspecialchars($_SESSION['user_nom']) ?></a>           
+                            <a class ="nav-link btn text-white px-3 me-2 btn-sm" href="/account"><i class="fa-regular fa-circle-user"></i> <?= htmlspecialchars($_SESSION['user_nom']) ?></a>           
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link btn text-primary px-3 me-2 btn-sm" href="/parking/reserver.php"> <i class="fa-solid fa-square-parking"></i> Parking</a> 
+                            <a class="nav-link btn text-primary px-3 me-2 btn-sm" href="/parking/reserver"> <i class="fa-solid fa-square-parking"></i> Parking</a> 
                         </li>
                         <li class="nav-item">
                             <a class="nav-link btn text-primary-.text-info-emphasis px-3 me-2 btn-sm" href="/desks/reserver.php">Bureaux <i class="fa-solid fa-person-booth"></i></a>
