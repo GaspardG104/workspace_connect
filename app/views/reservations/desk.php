7d89277bae2ace0bd6bab376ad71f10a33cae76b
Guidetti Gaspard <gaspardguidetti@gmail.com>
2026-04-08 13:33:33 +0200

    selection nombre rep pour la récurrence

--- app/views/reservations/desk.php
+++ app/views/reservations/desk.php
@@ -121,18 +121,8 @@
 
                                     <div class="col-md-6">
                                         <label id="label-count" class="small fw-bold"> </label>
-                                        <select name="recurrence_count" id="recurrence_count" class="form-select form-select-sm">
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                            <option value="6">6</option>
-                                            <option value="7">7</option>
-                                            <option value="8">8</option>
-                                            <option value="9">9</option>
-                                            <option value="10">10</option>
-                                        </select>
+                                        <input type="number" name="recurrence_count" id="recurrence_count"
+                                            class="form-select form-select-sm" value="1" min="1">
                                     </div>
                                 </div>
                                 <div class="form-text mt-2 text-muted">
